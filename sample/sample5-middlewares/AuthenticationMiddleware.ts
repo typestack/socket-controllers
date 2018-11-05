1c832591e9afe55e40e781f9bc6c3883b1cb9ca8
Rustam Mamadaminov <rmamdaminov@gmail.com>
2018-11-05 14:44:09 +0500

    fix: correct sample 1

--- sample/sample5-middlewares/AuthenticationMiddleware.ts
+++ sample/sample5-middlewares/AuthenticationMiddleware.ts
@@ -2,7 +2,7 @@ import {Middleware} from "../../src/decorators";
 import {MiddlewareInterface} from "../../src/MiddlewareInterface";
 
 @Middleware()
-export class AuthenitificationMiddleware implements MiddlewareInterface {
+export class AuthenticationMiddleware implements MiddlewareInterface {
 
     use(socket: any, next: ((err?: any) => any)): any {
         console.log("authentication...");
