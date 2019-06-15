f2cc864548174c24b93940828f5734ff475ac744
Rustam <rmamdaminov@gmail.com>
2019-06-15 09:03:15 +0500

    fix samples

--- sample/sample7-use-middlewares/AuthenticationMiddleware.ts
+++ sample/sample7-use-middlewares/AuthenticationMiddleware.ts
@@ -3,7 +3,6 @@ import {MiddlewareInterface} from "../../src/MiddlewareInterface";
 export class AuthenticationMiddleware implements MiddlewareInterface {
 
     use(socket: any, next: ((err?: any) => any)): any {
-        console.log(socket);
         console.log("authentication...");
         next();
     }
