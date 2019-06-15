e4700a6081d09ee890359fec17e3fcd437b31084
Rustam <rmamdaminov@gmail.com>
2019-06-15 13:04:09 +0500

    changes

--- src/CurrentUserChecker.ts
+++ src/CurrentUserChecker.ts
@@ -1,5 +1,6 @@
+import {Socket} from "socket.io";
 
 /**
  * Special function used to get currently authorized user.
  */
-export type CurrentUserChecker = (socket: any) => Promise<any>|any;
+export type CurrentUserChecker = (socket: Socket) => Promise<any>|any;
