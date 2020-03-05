fd4026b6284610903e3ff93b1ffe095746dc9e58
Max Kalhama <kalhama.max@gmail.com>
2020-03-05 14:11:11 +0200

    https://github.com/typestack/socket-controllers/issues/28

--- src/SocketControllerExecutor.ts
+++ src/SocketControllerExecutor.ts
@@ -216,7 +216,7 @@ export class SocketControllerExecutor {
 
     private parseParamValue(value: any, paramMetadata: ParamMetadata) {
         try {
-            const parseValue = typeof value === "string" ? JSON.parse(value) : value;
+            const parseValue = typeof value !== "string" ? JSON.parse(value) : value;
             if (paramMetadata.reflectedType !== Object && paramMetadata.reflectedType && this.useClassTransformer) {
                 const options = paramMetadata.classTransformOptions || this.plainToClassTransformOptions;
                 return plainToClass(paramMetadata.reflectedType, parseValue, options);
